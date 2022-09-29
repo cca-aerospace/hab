0709c45b0f60c0f95b37e6c9116642625b26d9b8
Lohen Chen <64998077+Lohen-Chen@users.noreply.github.com>
2022-09-29 13:58:23 -0700

    Create sample.py
    sample code for sample data

---
+++ data/sample.py
@@ -0,0 +1,12 @@
+import plotly.graph_objects as go
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import pandas as pd
+df = pd.read_csv('https://raw.githubusercontent.com/cca-aerospace/hab/main/data/sample%20data%20-%20Sheet1.csv')
+fig = go.Figure([go.Scatter(x=df['Time'], y=df['O2 levels'])])
+app = dash.Dash(__name__)
+app.layout = html.Div([
+    dcc.Graph(id="line-chart", figure=fig),
+])
+app.run_server(host="0.0.0.0", debug=True)

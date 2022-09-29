2edcce93a5f45c02eedb2d0981447620ae526cfe
Lohen Chen <64998077+Lohen-Chen@users.noreply.github.com>
2022-09-29 13:59:11 -0700

    Update main.py

--- data/main.py
+++ data/main.py
@@ -4,7 +4,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
 df = pd.read_csv('') #put csv file link here
-fig = go.Figure([go.Scatter(x=df['Date'], y=df['AAPL.High'])])
+fig = go.Figure([go.Scatter(x=df['foo'], y=df['foo'])])
 app = dash.Dash(__name__)
 app.layout = html.Div([
     dcc.Graph(id="line-chart", figure=fig),
